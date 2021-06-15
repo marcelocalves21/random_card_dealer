04e290e3c7ea92386d898292e3eaa16cad77442d
Marcelo C Alves <51497235+marcelocalves21@users.noreply.github.com>
2021-06-15 10:48:18 -0400

    Update on index.html and app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const changeCard = () => {
+  let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  let cardSymbol = [1, 2, 3, 4]
+  let randomCard = Math.floor(Math.random() * 14)
+  let randomSymbol = Math.floor(Math.random() * 4)
+  let finalCard = [cardNumbers[randomCard], cardSymbol[randomSymbol]]
+  document.querySelector('#number').textContent = finalCard[0]
+}
