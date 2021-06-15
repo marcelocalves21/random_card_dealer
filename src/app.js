8c21d175d843fa693f97e7026dbc049fc5c2b948
Marcelo C Alves <51497235+marcelocalves21@users.noreply.github.com>
2021-06-15 15:18:46 -0400

    Update
    Change images
    Finished JS and CSS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,39 @@
 
-const changeCard = () => {
-  let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-  let cardSymbol = [1, 2, 3, 4]
+const changeNumber = () => {
+  let allCardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
   let randomCard = Math.floor(Math.random() * 14)
+  let cardNumber = allCardNumbers[randomCard]
+  document.querySelector('.number').textContent = cardNumber
+}
+
+const changeImage = () =>{
+  let allCardSymbol = [1, 2, 3, 4]
   let randomSymbol = Math.floor(Math.random() * 4)
-  let finalCard = [cardNumbers[randomCard], cardSymbol[randomSymbol]]
-  document.querySelector('#number').textContent = finalCard[0]
+  let cardSymbol = allCardSymbol[randomSymbol]
+  switch (cardSymbol){
+    case 0 :
+      document.querySelector('.img').src='./assets/img/heart 200x200.png'
+      document.querySelector('.img2').src='./assets/img/heart 200x200.png'
+      break
+    case 1 :
+      document.querySelector('.img').src='./assets/img/diamond 200x200.png'
+      document.querySelector('.img2').src='./assets/img/diamond 200x200.png'
+      break
+    case 2 :
+      document.querySelector('.img').src='./assets/img/spades 200x200.png'
+      document.querySelector('.img2').src='./assets/img/spades 200x200.png'
+      break
+    case 3 :
+      document.querySelector('.img').src='./assets/img/clubs 200x200.png'
+      document.querySelector('.img2').src='./assets/img/clubs 200x200.png'
+      break
+    default:
+      break
+
+  }
+}
+
+const changeCard = () =>{
+  changeNumber()
+  changeImage()
 }
